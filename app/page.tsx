3d5715e2377de9b20baa73cb069c398669fbd18e
Grenish rai <107925840+Grenish@users.noreply.github.com>
2025-01-31 21:48:37 +0530

    Refactor login page UI and add form validation

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,37 @@
-import Image from "next/image";
+"use client";
+
+import { CircleUser, AtSign, Key } from "lucide-react";
 
 export default function Home() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className="min-h-screen w-full flex flex-col justify-center items-center">
+      <p className="text-center text-gray-600 text-sm mb-6 w-1/6">
+        Enter your credentials to access your account.
+      </p>
+      <form action="" className="relative flex flex-col w-1/2 items-center">
+        <div className="flex w-1/2 p-2 gap-2 items-center border-2 rounded-xl">
+          <AtSign className="w-6 h-6 text-gray-500" />
+          <input
+            type="text"
+            className="outline-none bg-transparent"
+            placeholder="College email"
+          />
+        </div>
+        <div className="flex w-1/2 p-2 gap-2 items-center border-2 rounded-xl mt-2">
+          <Key className="w-6 h-6 text-gray-500" />
+          <input
+            type="password"
+            className="outline-none bg-transparent"
+            placeholder="Password"
+          />
+        </div>
+        <p className="text-xs p-0.5 w-1/2">
+          For new student password is their registration number
+        </p>
+        <button className="bg-black hover:bg-black/80 text-white cursor-pointer rounded-lg px-4 py-2 mt-2 w-1/2">
+          Sign In
+        </button>
+      </form>
+    </div>
+  );
 }
