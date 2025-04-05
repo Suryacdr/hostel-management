2fdeba0ca30f915194bb7d6b9f41dfa424449960
Surya Joshi <suryajoshi40@gmail.com>
2025-04-05 22:22:35 +0530

    update

---
+++ app/notice-board/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import { ThemeProvider } from "@/hooks/ThemeProvider";
+
+const Pop = Poppins({
+    weight: ["400", "500", "600", "700"],
+    display: "swap",
+    subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+    title: "HostelHive - Dashboard",
+    description: "Get to know your hostel better",
+};
+
+export default function RootLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <div className={`${Pop.className} antialiased`}>
+            <ThemeProvider>{children}</ThemeProvider>
+        </div>
+    );
+}
---
+++ app/notice-board/page.tsx
@@ -0,0 +1,7 @@
+export default function NoticeBoard() {
+    return (
+        <div className="">
+            <h2>Notice Board</h2>
+        </div>
+    )
+}
